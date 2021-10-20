3929592bfab239031fe833b29f17a2559f57f250
Austin Harrison <austin_alt22@todds-mbp.msdomain.indiancreekschool.com>
2021-10-20 11:58:12 -0400

    defined team constructor and get team overall

--- .github/Team.java
+++ .github/Team.java
@@ -1,3 +1,18 @@
 public class Team {
-    
+    private ArrayList<Player> team;
+
+    public Team(ArrayList<Player> t){
+        team = t;
+    }
+
+    public int getTeamOverall(){ //returns team overall
+        int sum = 0;
+        int average = 0;
+        for(Player p: team){
+            sum += p.getOverall();
+        }
+        average = (int)(sum/team.size()+0.5);
+        return average;
+    }
+
 }
