8a6dc7c20a2d76ca39e9d3ff7645d845f80c106a
Benny Carter <bennycarter@bennys-mbp.msdomain.indiancreekschool.com>
2021-10-20 12:00:59 -0400

    add player consructors

--- .github/SF.java
+++ .github/SF.java
@@ -1,15 +1,15 @@
 public class SF extends Player{
 
     public SF(){
-        super(Random.getRandomInt(70,100),
-        Random.getRandomInt(50,91),
-        Random.getRandomInt(60,85),
-        Random.getRandomInt(70,90),
-        Random.getRandomInt(70,100),
-        Random.getRandomInt(30,45),
-        Random.getRandomInt(30,45),
+        super(Random.getRandomInt(50,75),
+        Random.getRandomInt(50,80),
         Random.getRandomInt(75,100),
-        Random.getRandomInt(70,100));
+        Random.getRandomInt(70,85),
+        Random.getRandomInt(50,75),
+        Random.getRandomInt(50,70),
+        Random.getRandomInt(50,70),
+        Random.getRandomInt(50,70),
+        Random.getRandomInt(45,60));
     }
     
 }
