d3b69eb7c0eb4b949caf0efd6b0e77aad83e5afc
Malcolm Morris-Berlin <73406050+MalcolmMorrisBerlin@users.noreply.github.com>
2021-10-20 09:20:34 -0700

    Update BallsApp.java
    jolijadfdf

--- .github/BallsApp.java
+++ .github/BallsApp.java
@@ -2,5 +2,5 @@ public class BallsApp {
     //main class that will run
     public static void main(String args[]){
         System.out.println(Random.newName());
-    }
+    }//sal;kjfaskdl;jfasd
 }
