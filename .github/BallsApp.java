6d79177642963c2dd62e5fa04ac53f3c93c2c3b2
Austin Harrison <austin_alt22@todds-mbp.msdomain.indiancreekschool.com>
2021-10-21 12:13:08 -0400

    added classes

--- .github/BallsApp.java
+++ .github/BallsApp.java
@@ -2,5 +2,9 @@ public class BallsApp {
     //main class that will run
     public static void main(String args[]){
         System.out.println(Random.newName());
+
+        PG bob = new PG();
+        System.out.println(bob.getOverall());
+        System.out.println(bob.getThree());
     }
 }
