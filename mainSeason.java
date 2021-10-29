78aa59889a138f459ccbfd613072a1414051df37
Austin Harrison <austin_alt22@Todds-MacBook-Pro.local>
2021-10-29 09:28:32 -0400

    worked on win-loss algorithm

--- mainSeason.java
+++ mainSeason.java
@@ -7,16 +7,45 @@ public class mainSeason{
    public static Team myTeam;
    public static Team tradeTeam;
 
-    public static boolean simGame(Team t1, Team t2){
-       gamecount++;
-       Team home=t1;
-       Team away=t2;
-       if(home.getTeamOverall() > away.getTeamOverall()){
-           return true;
-       } else {
-          return false;
+   public static boolean simGame(Team t1, Team t2){
+      gamecount++;
+
+      Team home=t1;
+      Team away=t2;
+
+      int ovr1 = home.getTeamOverall();
+      int ovr2 = away.getTeamOverall();
+
+      //home court advantage
+      //will determine home team (ignore names)
+
+      int determineHome = (int)(Math.random()*2+1.5);
+      if(determineHome == 1) {
+         ovr1 += 1;
+      } else {
+         ovr2 += 1;
+      }
+
+      if(ovr1 > ovr2){
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   //gives boost to a random team on any given day
+   public static Team anyGivenDay(Team t1, Team t2){
+       int teamTradingWith = (int)(Math.random()*(2)+0.5);
+       if(teamTradingWith == 1){
+         return t1;
        }
-    }
+       if(teamTradingWith == 2){
+         return t2;
+       }
+       return null;
+   }
+
+
 
    public static Team generateRandomTeam(Team t1, Team t2, Team t3, Team t4){
       int teamTradingWith = (int)(Math.random()*(4)+1.5);
