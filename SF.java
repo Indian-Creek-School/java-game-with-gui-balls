c9ec1f117674880d346c7609d4a9839c9e39c18b
Benny Carter <bennycarter@bennys-mbp.msdomain.indiancreekschool.com>
2021-11-03 15:14:33 -0400

    i

--- SF.java
+++ SF.java
@@ -20,8 +20,8 @@ public class SF extends Player{
         average = (int)(0.5*this.getHandle()+
         1.25*this.getClose()+
         1*this.getHeight()+
-        1*this.getIQ()+
-        1.5*this.getMid()+
+        0.5*this.getIQ()+
+        2*this.getMid()+
         0.5*this.getPlaymaking()+
         1.25*this.getSize()+
         1*this.getThree()+
