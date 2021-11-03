c5fcba444fac7ee290b635f9a3be48f0ff7802b2
Benny Carter <bennycarter@bennys-mbp.msdomain.indiancreekschool.com>
2021-11-03 11:55:45 -0400

    sg

--- SG.java
+++ SG.java
@@ -17,13 +17,13 @@ public class SG extends Player{
     public int determineOverall(){
         int average = 0;
         average = (int)(1*this.getHandle()+
-        0.5*this.getClose()+
+        0.75*this.getClose()+
         0.5*this.getHeight()+
         1.25*this.getIQ()+
         1.25*this.getMid()+
         1.25*this.getPlaymaking()+
         0.5*this.getSize()+
-        1.5*this.getThree()+
+        1.25*this.getThree()+
         1.25*this.getSpeed())/9;
 
         return average;
