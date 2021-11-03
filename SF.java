0008e93541df9051dd10860100ce7ac566f54be0
Benny Carter <bennycarter@bennys-mbp.msdomain.indiancreekschool.com>
2021-11-03 15:16:32 -0400

    q

--- SF.java
+++ SF.java
@@ -5,10 +5,10 @@ public class SF extends Player{
     public SF(){
         super(Random.getRandomInt(50,75),
         Random.getRandomInt(50,80),
-        Random.getRandomInt(85,100),
-        Random.getRandomInt(70,85),
-        Random.getRandomInt(50,75),
+        Random.getRandomInt(85,90),
         Random.getRandomInt(70,80),
+        Random.getRandomInt(50,75),
+        Random.getRandomInt(70,75),
         Random.getRandomInt(50,70),
         Random.getRandomInt(50,70),
         Random.getRandomInt(45,60));
